76ef921dcaf2e13fdb55b5fbe93622f75a65820f
RedwanAhmedTapuu <redwan25880@gmail.com>
2024-01-18 01:44:55 +0600

    frontend updated

--- src/components/RickshawpullerDashboard.jsx
+++ src/components/RickshawpullerDashboard.jsx
@@ -17,7 +17,26 @@ const RickshawpullerDashboard = () => {
 
   useEffect(() => {
     // Replace with your Socket.IO server URL
-  
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`${server}/rickshawpuller/details`, {
+          params: {
+            nid: userNid,
+          },
+        });
+
+        if (res) {
+          setRickshawpullerDetails(res.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    };
+
+    fetchData(); // Call the function immediately
+
+    console.log("Fetching data...");
+
     const emitRickshawmamaLocation = async () => {
       try {
         const position = await getCurrentLocation();
@@ -28,16 +47,6 @@ const RickshawpullerDashboard = () => {
           lat: latitude,
           lon: longitude,
         });
-        const res = await axios.get(`${server}/rickshawpuller/details`, {
-          params: {
-            nid: userNid,
-          },
-        });
-        if(res){
-          setRickshawpullerDetails(res.data)
-        }
-        
-        console.log(res);
       } catch (error) {
         console.error("Error getting rickshawmama location:", error.message);
       }
@@ -45,8 +54,7 @@ const RickshawpullerDashboard = () => {
 
     emitRickshawmamaLocation();
 
-    socket.on("connect", () => {
-    });
+    socket.on("connect", () => {});
 
     socket.on("rickshawPullerLocationUpdate", async (data) => {
       console.log("Rickshawpuller location updated:", data);
@@ -67,36 +75,34 @@ const RickshawpullerDashboard = () => {
 
   return (
     <div className="container mx-auto p-8 w-full h-screen flex_center">
-        <div className="w-96 h-96 sm:w-[60%] flex-col-center  bg-gradient-to-r from-blue-500 to-indigo-700 p-8 rounded-lg shadow-lg text-white">
-          <h1 className="text-3xl font-bold mb-4 text-center">
-            Rickshawpuller Dashboard
-          </h1>
+      <div className="w-96 h-96 sm:w-[60%] flex-col-center  bg-gradient-to-r from-blue-500 to-indigo-700 p-8 rounded-lg shadow-lg text-white">
+        <h1 className="text-3xl font-bold mb-4 text-center">
+          Rickshawpuller Dashboard
+        </h1>
 
-          {rickshawpullerDetails && (
-            <div className="max-[600px]:flex_col_center sm:flex sm:justify-center lg:items-center space-x-4 lg:space-x-44 w-full">
-              {rickshawpullerDetails.image && (
-                <img
-                  src={rickshawpullerDetails.image}
-                  alt="Rickshawpuller"
-                  className="w-44 h-44 lg:w-72 lg:h-72 rounded-full border-4 border-white"
-                />
-              )}
-              <div>
-                <p className="text-lg max-[500px]:text-[1rem] sm:text-xl font-bold">
-                  Name: {rickshawpullerDetails.name || "N/A"}
-                </p>
-                <p className="text-lg max-[500px]:text-[1rem] sm:text-xl font-bold">
-
-                  Phone: {rickshawpullerDetails.phone || "N/A"}
-                </p>
-                <p className="text-lg max-[500px]:text-[1rem] sm:text-xl font-bold">
-
-                  NID: {rickshawpullerDetails.nid || "N/A"}
-                </p>
-              </div>
+        {rickshawpullerDetails && (
+          <div className="max-[600px]:flex_col_center sm:flex sm:justify-center lg:items-center space-x-4 lg:space-x-44 w-full">
+            {rickshawpullerDetails.image && (
+              <img
+                src={rickshawpullerDetails.image}
+                alt="Rickshawpuller"
+                className="w-44 h-44 lg:w-72 lg:h-72 rounded-full border-4 border-white"
+              />
+            )}
+            <div>
+              <p className="text-lg max-[500px]:text-[1rem] sm:text-xl font-bold">
+                Name: {rickshawpullerDetails.name || "N/A"}
+              </p>
+              <p className="text-lg max-[500px]:text-[1rem] sm:text-xl font-bold">
+                Phone: {rickshawpullerDetails.phone || "N/A"}
+              </p>
+              <p className="text-lg max-[500px]:text-[1rem] sm:text-xl font-bold">
+                NID: {rickshawpullerDetails.nid || "N/A"}
+              </p>
             </div>
-          )}
-        </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
